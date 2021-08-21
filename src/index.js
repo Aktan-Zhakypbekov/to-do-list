9188a7b77d0a39331dfeda81f1b1454fb0346e02
Aktan Zhakypbekov <aktanzhakypbekovbusiness@gmail.com>
2021-08-21 18:43:37 +0600

    ...

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
-import { projectInterface } from "./projectInterface";
+import { projectInterface, removeProjectInterfaces } from "./projectInterface";
 
 let inboxButton = document.querySelector("#inboxButton");
 inboxButton.addEventListener("click", (e) => {
-  projectInterface();
+  removeProjectInterfaces();
+  projectInterface("Inbox");
+});
+
+let todayButton = document.querySelector("#todayButton");
+todayButton.addEventListener("click", (e) => {
+  removeProjectInterfaces();
+  projectInterface("Today");
+});
+
+let thisWeekButton = document.querySelector("#thisWeekButton");
+thisWeekButton.addEventListener("click", (e) => {
+  removeProjectInterfaces();
+  projectInterface("This Week");
+});
+
+let toDoFormContainer = document.querySelector("#toDoFormContainer");
+let exitButton = document.querySelector("#exitButton");
+exitButton.addEventListener("click", (e) => {
+  toDoFormContainer.style.cssText = "display: none";
 });
--- src/projectInterface.js
+++ src/projectInterface.js
@@ -1,11 +1,11 @@
-function projectInterface() {
+function projectInterface(projectHeaderName) {
   let projectMainContainer = document.createElement("div");
   projectMainContainer.className = "projectMainContainer";
   projectMainContainer.style.cssText =
     "width: 100%; height: 100%; background-color: blue";
 
   let projectHeader = document.createElement("div");
-  projectHeader.textContent = "Project Header";
+  projectHeader.textContent = projectHeaderName;
   projectHeader.className = "projectHeader";
   projectHeader.style.cssText =
     "width: 100%; height: 50px; background-color: darkblue";
@@ -16,10 +16,23 @@ function projectInterface() {
   projectAddButton.style.cssText =
     "width: 100%; height: 50px; background-color: red; border: none";
 
+  let toDoFormContainer = document.querySelector("#toDoFormContainer");
+  projectAddButton.addEventListener("click", (e) => {
+    toDoFormContainer.style.cssText = "display: grid";
+  });
+
   projectMainContainer.appendChild(projectHeader);
   projectMainContainer.appendChild(projectAddButton);
   let display = document.querySelector("#display");
   display.appendChild(projectMainContainer);
 }
+function removeProjectInterfaces() {
+  let projectMainContainerList = document.querySelectorAll(
+    ".projectMainContainer"
+  );
+  projectMainContainerList.forEach((element) => {
+    element.remove();
+  });
+}
 
-export { projectInterface };
+export { projectInterface, removeProjectInterfaces };
